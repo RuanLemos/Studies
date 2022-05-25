93acc761f6176ebf0cb667bc26d4f5724d5d3cff
Ruan Lemos <71831848+RuanLemos@users.noreply.github.com>
2022-05-25 09:19:56 -0300

    Create Musica.java

---
+++ Jukebox/Musica.java
@@ -0,0 +1,29 @@
+public class Musica {
+    private String titulo;
+    private String compositor;
+    private String duracao;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCompositor() {
+        return compositor;
+    }
+
+    public void setCompositor(String compositor) {
+        this.compositor = compositor;
+    }
+
+    public String getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(String duracao) {
+        this.duracao = duracao;
+    }
+}
